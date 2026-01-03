a0ee0175cc0ddc7bdd2206e2891799219601045c
Danny <byakkoroyale@gmail.com>
2026-01-03 21:42:51 +0100

    -

--- ZodiacEnemies.lua
+++ ZodiacEnemies.lua
@@ -112,9 +112,10 @@ end
 
 for i, enemy in ipairs(enemyPositions) do
 
-    while Svc.ClientState.TerritoryType ~= enemy.zoneId do
+    while Svc.ClientState.TerritoryType ~= enemy.zoneId and not Svc.Condition[27] do
         IPC.Lifestream.Teleport(enemy.aetheryiteId, 0)
-        yield("/wait 5")
+        Dalamud.Log("Teleporting")
+        yield("/wait 15")
     end
 
     if not Svc.Condition[4] then
